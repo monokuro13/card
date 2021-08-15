8361c5673c899ace5ac3c1c748f53d744adf17a5
monokuro13 <aikoraikida0622@gmail.com>
2021-08-15 20:57:52 +0900

    新規投稿機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
   get "/" => "home#top"
   get "about" => "home#about"
-  get "posts/index" => "posts#index"
-  
 end

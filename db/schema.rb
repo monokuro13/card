e184c72ad82db625ddd5fe0e8bc9def06f97e1b9
monokuro13 <aikoraikida0622@gmail.com>
2021-08-21 23:47:01 +0900

    投稿一覧取得、投稿編集アクセス権限

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_125131) do
+ActiveRecord::Schema.define(version: 2021_08_21_140945) do
 
   create_table "posts", charset: "utf8mb3", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|

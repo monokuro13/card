b69e83aae7fb18e0757d18ddc2b9233cd64d5082
monokuro13 <aikoraikida0622@gmail.com>
2021-09-17 23:13:34 +0900

    モデル、コントローラーテストファイル作成

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /index" do
+    it "responds successfully" do
+      get '/users/index'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end

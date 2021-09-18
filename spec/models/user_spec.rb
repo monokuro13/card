724df32f242e6212474806770e5d7c7ad285d2e7
monokuro13 <aikoraikida0622@gmail.com>
2021-09-18 23:44:11 +0900

    User,Post index テスト、User新規登録昨日のテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a email,password and name" do
+    user = User.new(
+      name: "user1",
+      email: "user1@user.com",
+      password: "user1pass",
+    )
+    expect(user).to be_valid
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,4 +7,5 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
 end
